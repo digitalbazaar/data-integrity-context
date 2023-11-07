bf87c05c8af285766b8e0fa69cbe7bd733c7cdc8
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-11-07 14:34:28 -0500

    Export versioned context URLs and update default context to contextV2.

--- js/constants.js
+++ js/constants.js
@@ -1,10 +1,13 @@
 /*!
- * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Digital Bazaar, Inc. All rights reserved.
  */
 'use strict';
 
 module.exports = {
-  CONTEXT_FILENAME: 'data-integrity-v1.jsonld',
-  CONTEXT_URL: 'https://w3id.org/security/data-integrity/v1',
+  CONTEXT_FILENAME_V1: 'data-integrity-v1.jsonld',
+  CONTEXT_FILENAME_V2: 'data-integrity-v2.jsonld',
+  DATA_INTEGRITY_CONTEXT_V1_URL: 'https://w3id.org/security/data-integrity/v1',
+  DATA_INTEGRITY_CONTEXT_V2_URL: 'https://w3id.org/security/data-integrity/v2',
+  CONTEXT_URL: 'https://w3id.org/security/data-integrity/v2',
   CBORLD_VALUE: 0x30
 };
--- js/index.js
+++ js/index.js
@@ -1,14 +1,19 @@
 /*!
- * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Digital Bazaar, Inc. All rights reserved.
  */
 'use strict';
 
-const context = require('./context');
+const contextV1 = require('./data-integrity-v1');
+const contextV2 = require('./data-integrity-v2');
 const constants = require('./constants');
-const {CONTEXT_URL, CBORLD_VALUE} = constants;
+const {
+  CONTEXT_URL, DATA_INTEGRITY_CONTEXT_V1_URL, DATA_INTEGRITY_CONTEXT_V2_URL,
+  CBORLD_VALUE
+} = constants;
 
 const contexts = new Map();
-contexts.set(CONTEXT_URL, context);
+contexts.set(DATA_INTEGRITY_CONTEXT_V1_URL, contextV1);
+contexts.set(DATA_INTEGRITY_CONTEXT_V2_URL, contextV2);
 
 const appContextMap = new Map();
 appContextMap.set(CONTEXT_URL, CBORLD_VALUE);
@@ -17,6 +22,8 @@ module.exports = {
   constants,
   contexts,
   appContextMap,
+  DATA_INTEGRITY_CONTEXT_V1_URL,
+  DATA_INTEGRITY_CONTEXT_V2_URL,
   CONTEXT_URL,
-  CONTEXT: context
+  CONTEXT: contextV2
 };
